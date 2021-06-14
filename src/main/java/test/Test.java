26f035424154635620035f616a7218398e4e9d56
liuxiaoliang <762605241@qq.com>
2021-06-14 12:17:14 +0800

    总结

---
+++ src/main/java/test/Test.java
@@ -0,0 +1,10 @@
+package test;
+
+import java.util.Deque;
+import java.util.Stack;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(Integer.toBinaryString(0xff00));
+    }
+}
