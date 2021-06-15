251d1dbd10e441e7ae8ac4319a70bbe82f30abfa
liuxiaoliang <762605241@qq.com>
2021-06-15 23:05:11 +0800

    面试题总结

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -5,6 +5,17 @@ import java.util.Stack;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println(Integer.toBinaryString(0xff00));
+        int a = 0;
+        int b = 0;
+        breakFlag1:
+        for (int i = 0; i < 33; i++) {
+            for (int j = 0; j < 55; j++) {
+                a++;
+                continue breakFlag1;
+            }
+            b++;
+        }
+        System.out.println(a);
+        System.out.println(b);
     }
 }
