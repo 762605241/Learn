ac873e6581515b56e8820a34644b68901c80dc06
liuxiaoliang <762605241@qq.com>
2021-06-17 22:39:48 +0800

    面试题总结

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -1,16 +1,10 @@
 package test;
 
-import java.util.Deque;
-import java.util.Stack;
+import java.util.*;
 
 public class Test {
     public static void main(String[] args) {
-        String str1 = "hello";
-        String str2 = new String("hello");
-        String str3 = "hello";
-        String str4 = new String("hello");
-        System.out.println(str1.equals(str2));
-        System.out.println(str2.equals(str4));
-
+        ArrayList arrayList = new ArrayList();
+        HashMap hashMap = new HashMap();
     }
 }
