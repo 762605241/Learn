596d411851fd42e61e30f7e6956bbf97cca63170
liuxiaoliang_P12 <762605241@qq.com>
2021-06-20 17:12:59 +0800

    总结

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -1,10 +1,17 @@
 package test;
 
 import java.util.*;
+import java.util.concurrent.SynchronousQueue;
 
 public class Test {
     public static void main(String[] args) {
         ArrayList arrayList = new ArrayList();
         HashMap hashMap = new HashMap();
+    }
+
+    class finalMethodTest {
+        private void test() {
+            System.out.println("1");
+        }
     }
 }
