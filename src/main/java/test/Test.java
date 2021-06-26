fb4db6ea39bf3327548ec654e891a28298e598bf
liuxiaoliang_P12 <762605241@qq.com>
2021-06-26 10:22:02 +0800

    总结

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -7,10 +7,11 @@ import java.util.concurrent.SynchronousQueue;
 
 public class Test {
     public static void main(String[] args) {
-        HashMap<Integer, Integer> hashMap = new HashMap<>(4);
-        hashMap.put(null, 1);
-        hashMap.put(null, 2);
-        System.out.println(hashMap.get(null));
+//        HashMap<Integer, Integer> hashMap = new HashMap<>(4);
+//        hashMap.put(null, 1);
+//        hashMap.put(null, 2);
+//        System.out.println(hashMap.get(null));
+        System.out.println(tableSizeFor(12));
     }
 
     class finalMethodTest {
@@ -18,6 +19,16 @@ public class Test {
         private void test() {
             System.out.println("1");
         }
+    }
+
+    static final int tableSizeFor(int cap) {
+        int n = cap - 1;
+        n |= n >>> 1;
+        n |= n >>> 2;
+        n |= n >>> 4;
+        n |= n >>> 8;
+        n |= n >>> 16;
+        return (n < 0) ? 1 : (n >= (1 << 30)) ? (1 << 30) : n + 1;
     }
 }
 
