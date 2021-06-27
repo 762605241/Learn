aa410a354f8a35e627b8fc05e671e3d4d232e159
liuxiaoliang <762605241@qq.com>
2021-06-27 16:36:20 +0800

    面试题总结

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -6,29 +6,6 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.SynchronousQueue;
 
 public class Test {
-    public static void main(String[] args) {
-//        HashMap<Integer, Integer> hashMap = new HashMap<>(4);
-//        hashMap.put(null, 1);
-//        hashMap.put(null, 2);
-//        System.out.println(hashMap.get(null));
-        System.out.println(tableSizeFor(12));
-    }
-
-    class finalMethodTest {
-        static final  String a = "111";
-        private void test() {
-            System.out.println("1");
-        }
-    }
-
-    static final int tableSizeFor(int cap) {
-        int n = cap - 1;
-        n |= n >>> 1;
-        n |= n >>> 2;
-        n |= n >>> 4;
-        n |= n >>> 8;
-        n |= n >>> 16;
-        return (n < 0) ? 1 : (n >= (1 << 30)) ? (1 << 30) : n + 1;
-    }
+    
 }
 
