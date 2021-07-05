d5bbe56e1dd8c6d16005dd2dc2ebc5f7402ce77e
liuxiaoliang_P12 <762605241@qq.com>
2021-07-05 17:47:10 +0800

    总结

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -1,11 +1,23 @@
 package test;
 
+import java.io.Externalizable;
 import java.io.ObjectOutputStream;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.SynchronousQueue;
 
 public class Test {
-    
+    public static void main(String[] args) {
+        int[] a = {1, 3, 4, 7};
+        test(a);
+        System.out.println(Arrays.toString(a));
+    }
+
+    private static void test(int[] s) {
+        s[0] = 99;
+        s = new int[]{9, 9};
+    }
+
+
 }
 
