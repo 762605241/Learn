2a7692dc0d5830119d41239d2b4a0ecca20cdb9d
liuxiaoliang_P12 <762605241@qq.com>
2021-07-06 18:38:40 +0800

    总结

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -1,21 +1,45 @@
 package test;
 
-import java.io.Externalizable;
-import java.io.ObjectOutputStream;
-import java.util.*;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.SynchronousQueue;
 
 public class Test {
     public static void main(String[] args) {
-
+        System.out.println(getSubStringIndex("acabc", "ab"));
     }
 
-    private static void test(int[] s) {
-        s[0] = 99;
-        s = new int[]{9, 9};
+    public static int getSubStringIndex(String str, String subStr) {
+        int strLength = str.length();
+        int subStrLength = subStr.length();
+        char[] strArray = str.toCharArray();
+        char[] subStrArray = subStr.toCharArray();
+        boolean isFind = false;
+        for (int i = 0; i < strLength; i++) {
+            int j = 0;
+            if (strArray[i] == subStrArray[j]) {
+                int m = i;
+                while (m < strLength && j < subStrLength) {
+                    if (strArray[m] == subStrArray[j]) {
+                        m++;
+                        j++;
+                        isFind = true;
+                    } else {
+                        isFind = false;
+                        break;
+                    }
+                }
+                if (isFind) {
+                    return i;
+                }
+            }
+        }
+        return -1;
     }
-
-
 }
+
+
+
+
+
+
+
+
 
